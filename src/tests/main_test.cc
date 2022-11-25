02bd135ed4ed65002bf79f645df8b7956d3f3efd
Hubert Furr <hubertfu@student.21-school.ru>
2022-11-25 23:37:47 +0300

    add todo

--- src/tests/main_test.cc
+++ src/tests/main_test.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
   // Задаем фильтры для запуска некоторых тестов
-  ::testing::GTEST_FLAG(filter) = "*TestMutators*";
+  // ::testing::GTEST_FLAG(filter) = "*TestInverse*";
 
   // Передаем параметры командной строки в gtest
   testing::InitGoogleTest(&argc, argv);
@@ -10,3 +10,13 @@ int main(int argc, char *argv[]) {
   // Запускаем все тесты
   return RUN_ALL_TESTS();
 }
+  // TODO:
+  // S21Matrix& operator+=(const S21Matrix& other);
+  // S21Matrix& operator-=(const S21Matrix& other);
+  // S21Matrix& operator*=(const S21Matrix& other);
+  // S21Matrix& operator*=(const double& number);
+  // S21Matrix& operator=(const S21Matrix& other);
+
+  // Конструктор переноса через присваивание
+
+  // Дефолтная матрица 0х0
